82b955e198bc16f1cb0572d743bfa635eef0d8c7
Sara Kane <7584520+sarakane@users.noreply.github.com>
2020-11-10 15:50:35 -0800

    Change document title and remove unneeded files

--- webapps/ch2-flight-search-fontend/src/index.js
+++ webapps/ch2-flight-search-fontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
@@ -28,7 +27,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
