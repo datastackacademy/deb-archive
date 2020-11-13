bd90f7ce261e39eba01840f553ac89a87ca6a667
Sara Kane <7584520+sarakane@users.noreply.github.com>
2020-11-13 13:23:49 -0800

    Add function that sets state hooks and local storage for search query properties to previous valid query data when clicking on previous search

--- webapps/ch2-flight-search-fontend/src/pages/Search.jsx
+++ webapps/ch2-flight-search-fontend/src/pages/Search.jsx
@@ -8,9 +8,22 @@ import ls from "local-storage";
 const Search = (props) => {
   const previousValidQuery = ls.get("previousQuery")? ls.get("previousQuery") : "";
 
+  const handleClick = () => {
+    props.setSrc(previousValidQuery.src);
+    ls.set("src", previousValidQuery.src);
+    props.setDest(previousValidQuery.dest);
+    ls.set("dest", previousValidQuery.dest);
+    props.setStartDate(previousValidQuery.start);
+    ls.set("startDate", previousValidQuery.start);
+    props.setEndDate(previousValidQuery.end);
+    ls.set("endDate", previousValidQuery.end);
+
+    window.location.href = '/results'
+  }
+
   return (
     <div className="page-container">
-      {previousValidQuery.src && previousValidQuery.dest && previousValidQuery.start && previousValidQuery.end && <em className="previous-search" onClick={() => window.location.href = '/results'}>Previous Search: {previousValidQuery.src} to {previousValidQuery.dest} from {formatDate(previousValidQuery.start, "MM/DD/YYYY")} to {formatDate(previousValidQuery.end, "MM/DD/YYYY")}</em>}
+      {previousValidQuery.src && previousValidQuery.dest && previousValidQuery.start && previousValidQuery.end && <em className="previous-search" onClick={handleClick}>Previous Search: {previousValidQuery.src} to {previousValidQuery.dest} from {formatDate(previousValidQuery.start, "MM/DD/YYYY")} to {formatDate(previousValidQuery.end, "MM/DD/YYYY")}</em>}
       <br></br>
       <MainForm {...props} />
     </div>)
