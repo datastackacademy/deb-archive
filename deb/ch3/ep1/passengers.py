d14918f96efe8b39e67b238e28f82774a1dc0f59
dnielsen <dnielsen@zdatainc.com>
2020-11-17 10:21:34 -0800

    added full name to script

--- deb/ch3/ep1/passengers.py
+++ deb/ch3/ep1/passengers.py
@@ -14,7 +14,16 @@ passengers_df = passengers_df.withColumn('first_name', initcap(col('first_name')
 .withColumn('middle_name', initcap(col('middle_name')))\
 .withColumn('last_name', initcap(col('last_name')))
 
+# Create full_name
+passengers_df = passengers_df.withColumn('full_name',
+                                         concat_ws(" ",
+                                                    col('first_name'),
+                                                    col('middle_name'),
+                                                    col('last_name')))
+
 # Create a sha2 uid based on all the columns of the dataframe
-passengers_df = passengers_df.withColumn('uid', sha2(concat_ws("|", *passengers_df.columns), 256))
+passengers_df = passengers_df.withColumn('uid',
+                                         sha2(concat_ws("|", *passengers_df.columns),
+                                              256))
 
 passengers_df.write.parquet(save_path)
