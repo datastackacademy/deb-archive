cc9ab9f7b6624486b625f978ee61ac6ceac1ac53
dnielsen <dnielsen@zdatainc.com>
2020-11-17 21:23:44 -0800

    wrapped script in functions; doesn't seem to warrent a class to me

--- deb/ch3/ep1/answers/passengers.py
+++ deb/ch3/ep1/answers/passengers.py
@@ -2,30 +2,39 @@ from deb.utils.config import config
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import initcap, col, sha2, concat_ws
 
+# ===============================================================
+# ‹‹=‹‹=‹‹=‹‹=‹‹=‹‹=‹‹=‹‹= RUN FUNCTIONS =››=››=››=››=››=››=››=››
+# ===============================================================
 
-# Build session and read the csv into a pypsark dataframe
-sparkql = SparkSession.builder.master('local').getOrCreate()
-people_path = config['defaults']['ch3']['ep1']['passenger_input'].get(str)
-save_path = config['defaults']['ch3']['ep1']['passenger_output'].get(str)
-passengers_df = sparkql.read.csv(people_path, header=True)
+def run():
+    # Build session
+    sparkql = SparkSession.builder.master('local').getOrCreate()
 
-# Load the passenger data and make sure the names have initial capitalization
-passengers_df = passengers_df.withColumn('first_name', initcap(col('first_name')))\
-.withColumn('middle_name', initcap(col('middle_name')))\
-.withColumn('last_name', initcap(col('last_name')))
+    # Load config informaiton if __name__ == '__main__':
+    people_path = config['defaults']['ch3']['ep1']['passenger_input'].get(str)
+    save_path = config['defaults']['ch3']['ep1']['passenger_output'].get(str)
+    passengers_df = sparkql.read.csv(people_path, header=True)
 
-# Create full_name column
-passengers_df = passengers_df.withColumn('full_name',
-                                         concat_ws(" ",
-                                                    col('first_name'),
-                                                    col('middle_name'),
-                                                    col('last_name')))
+    # Load the passenger data and make sure the names have initial capitalization
+    passengers_df = passengers_df.withColumn('first_name', initcap(col('first_name')))\
+                                 .withColumn('middle_name', initcap(col('middle_name')))\
+                                 .withColumn('last_name', initcap(col('last_name')))
 
-# Create a sha2 uid based on the email
-passengers_df = passengers_df.withColumn('uid',
-                                         sha2(concat_ws("|", col('email')),
-                                              256))
+    # Create full_name column
+    passengers_df = passengers_df.withColumn('full_name',
+                                             concat_ws(" ",
+                                                       col('first_name'),
+                                                       col('middle_name'),
+                                                       col('last_name')))
 
-# Save dataframe as a parquet file
-passengers_df.write.parquet(save_path)
+    # Create a sha2 uid based on the email
+    passengers_df = passengers_df.withColumn('uid',
+                                             sha2(concat_ws("|", col('email')),
+                                                  256))
 
+    # Save dataframe as a parquet file
+    passengers_df.write.parquet(save_path)
+
+    
+if __name__ == '__main__':
+    run()
