a8893d5f783aff9802efbe22d31f3b1d5cd89dcf
Sara Kane <7584520+sarakane@users.noreply.github.com>
2020-11-18 16:24:22 -0800

    Add array of colors for flights per day graph

--- webapps/ch2-flight-search-fontend/src/helpers/flightsPerDay.js
+++ webapps/ch2-flight-search-fontend/src/helpers/flightsPerDay.js
@@ -1,7 +1,5 @@
 import moment from 'moment';
-import { colorArray } from './checker'
 
-// Create an array of all dates from the start date to the end date inclusive
 const datesArray = (start, end) => {
   let dateArr = [];
   for(let currentDate = moment(start); currentDate.format('YYYY-MM-DD') <= end;) {
@@ -11,28 +9,37 @@ const datesArray = (start, end) => {
   return dateArr;
 }
 
-// Check if an airline doesn't have a count for a date in the date array and if not add a zero for that day
 const addZeroIfNoFlight = (counts, dates) => {
   let returnCounts = [];
   dates.forEach(date => {
     let formattedDate = moment(date).format("YYYY-MM-DD")
     !counts[formattedDate]? returnCounts.push(0): returnCounts.push(counts[formattedDate]);
   })
-  console.log(counts);
   return returnCounts;
 }
 
-
-
 const graphData = (groupedFlights, airlines, graphDates) => {
-  const colors = colorArray(groupedFlights, "b");
+
+  const colors = [
+  'rgba(10, 33, 51, 1)',
+  'rgba(44, 74, 99, 1)',
+  'rgba(11, 93, 156, 1)',
+  'rgba(48, 118, 172, 1)',
+  'rgba(43, 146, 224, 1)',
+  'rgba(62, 157, 230, 1)', 
+  'rgba(64, 179, 131, 1)',
+  'rgba(26, 142, 93, 1)',
+  'rgba(34, 89, 65, 1)',
+  'rgba(6, 48, 31, 1)'];
+
   return groupedFlights.map((airlineFlights, i) => {
     let returnObj = {};
     returnObj.label = airlines[airlineFlights.airline];
     returnObj.data = addZeroIfNoFlight(airlineFlights.flights_per_day_count, graphDates);
     returnObj.fill = false;
-    returnObj.borderColor = [colors[i]];
+    returnObj.borderColor = colors[i];
     returnObj.pointBackgroundColor = colors[i];
+    returnObj.lineTension = 0;
     return returnObj;
   });
 }
